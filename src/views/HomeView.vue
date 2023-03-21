2ff701be194f647a793f6095839557ebca41b301
Daanyy <daanyy_92@yahoo.com>
2023-03-21 18:25:26 +0200

    Vue js code cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>GitHub User Gists</h1>
   </main>
 </template>
+
+<script setup></script>
