a8a7b5f8f18d017ca234727caab385384bd6e847
Daanyy <daanyy_92@yahoo.com>
2023-03-21 19:37:01 +0200

    Implemented the SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="search-bar">
+        <input name="search-item" :value="modelValue" @input="handleInput">
+        <button @click="handleClick">Search</button>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    modelValue: {
+        type: String
+    }
+})
+
+const emit = defineEmits(['update:modelValue', 'click'])
+
+const handleInput = (event) => {
+    emit('update:modelValue', event.target.value)
+}
+
+const handleClick = () => {
+    emit('click')
+}
+</script>
